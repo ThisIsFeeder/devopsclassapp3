b617bfd36275b46b7f3a89205626f47fbf02c7f1
Sovannareach Darom <sovannareach_darom@yahoo.com>
2021-10-17 09:12:10 +0700

    change index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-	res.send("Hello dev app3");
+	res.send("Hello app3 from team tnout");
 });
 
 app.listen(8080,function(){
